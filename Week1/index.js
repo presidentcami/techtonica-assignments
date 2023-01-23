8e59d056552730b98c08c45e10a07b8e824dfda2
Camille Williams <camiwills325@gmail.com>
2023-01-23 15:51:28 -0500

    use dom manipulation to add a border to photo w mouseover

--- Week1/index.js
+++ Week1/index.js
@@ -2,3 +2,12 @@
 let newListItem = document.createElement('li');
 newListItem.textContent = 'A whole lot of love!';
 document.querySelector('ul').appendChild(newListItem);
+
+// Add event listener to put a border around the photo
+const picBorder = document.querySelector('img');
+
+function addBorder() {
+    picBorder.style.border = '5px solid red';
+}
+
+picBorder.addEventListener('mouseover', addBorder);
