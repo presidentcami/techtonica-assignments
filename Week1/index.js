0e8e79fecbb5f3015b22e49ce15d0f789e225daf
Camille Williams <camiwills325@gmail.com>
2023-01-27 16:00:36 -0500

    update checklist

--- Week1/index.js
+++ Week1/index.js
@@ -1,7 +1,22 @@
 // Add a new List Item
-let newListItem = document.createElement('li');
-newListItem.textContent = 'A whole lot of love!';
-document.querySelector('ul').appendChild(newListItem);
+
+// ****** THE COMMENTED OUT CODE & PSUEDOCODE WAS NOT WORKING ******
+// let newListItem = document.createElement('li');
+// newListItem.classList.add("ingredients");
+// newListItem.textContent = 'A whole lot of love!';
+// checkbox.value = newListItem;
+// make a new element with type checkbox
+// add that to newListItem with appendchild to newlistitem
+// class list to make newListItem.classList.add("ingredients")
+// checkbox.appendChild(newListItem);
+
+let checkbox = document.createElement('input');
+checkbox.type = "checkbox";
+let label = document.createElement('label');
+label.appendChild(document.createTextNode("A whole lot of love!"));
+document.getElementById('ingredientsChecklist').appendChild(checkbox);
+document.getElementById('ingredientsChecklist').appendChild(label);
+
 
 // Add event listener to put a border around the photo
 const picBorder = document.querySelector('img');
@@ -18,9 +33,33 @@ function addBorder() {
 picBorder.addEventListener('click', addBorder); 
 
 // Attempt to change the color of the text on click for links
-const hyperLinks = document.querySelector('a');
+// const hyperLinks = document.querySelectorAll('.links');
 
-function changeColor() {
-    hyperLinks.style.color = 'white';
-}
-hyperLinks.addEventListener('click', changeColor);
+// function mouseoverLink (textColor) {
+//     textColor = 'white';
+//     hyperLinks.style.color = textColor;
+//     console.log("we changed the color of text")
+// }
+
+// function linksListener(hyperLink) {
+//     hyperLink.addEventListener('mouseover', mouseoverLink)
+// }
+
+// hyperLinks.forEach(linksListener)
+
+// change function
+// function changeLinkColor (arrayOfLinks) {
+//     for (let i = 0; i < arrayOfLinks.length; i++) {
+//         arrayOfLinks.addEventListener('click', () => {
+//             arrayOfLinks[i].style.color = 'white';
+//         })
+//     }
+// }
+
+// changeLinkColor(hyperLinks);
+
+// make a for loop to go thru each hyperlink, to each link add event listener
+// hyperLinks.addEventListener('click', function(){
+//     for (let i = 0; i < hyperLinks.length; i++) {
+//         hyperLinks[i].style.color = 'white';
+// });
