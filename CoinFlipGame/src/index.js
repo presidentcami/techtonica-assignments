0b0563b5a2d9558488c7239326ee6fdccf0c289e
Camille Williams <camiwills325@gmail.com>
2023-02-09 15:51:14 -0500

    get button element in js

---
+++ CoinFlipGame/src/index.js
@@ -0,0 +1,9 @@
+/* need an event listener for the submit button 
+ on click, flip the coin - so the result section will
+show either true or false. the result should also check
+if the coin flip matched the answer, and the answer will
+say something like 'correct it's heads' or 'eh, wrong'
+
+ */
+
+const submitButton = document.getElementById('submit');
