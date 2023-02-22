f705866c1f1dd51600487c937a03e67fa70f4f0b
Camille Williams <camiwills325@gmail.com>
2023-02-22 17:22:23 -0500

    Add app.listen to start client

--- Week6API/index.js
+++ Week6API/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const PORT = 5001;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express')
 })
+
+app.listen(PORT, () => console.log(`MY hello world app listening on port ${PORT}!`));
