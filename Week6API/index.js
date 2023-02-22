35952b46814d74fdc5a8435020ff835238327618
Camille Williams <camiwills325@gmail.com>
2023-02-22 17:29:53 -0500

    Configure body parser middleware

--- Week6API/index.js
+++ Week6API/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5001;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express')
