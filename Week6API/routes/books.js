10debfab1f8b46ea3969eb5f40b901e4b6e31ec6
Camille Williams <camiwills325@gmail.com>
2023-02-23 12:22:10 -0500

    Add more books to books.json

--- Week6API/routes/books.js
+++ Week6API/routes/books.js
@@ -2,12 +2,25 @@ import express from 'express';
 
 const router = express.Router();
 
-const books = [ {
-    "isbn" : 12345,
-    "title" : "Who Fears Death",
-    "author" : "Nnedi Okorafor",
-    "format" : "Paperback"
-}
+const books = [ 
+        {
+            "isbn" : 12345,
+            "title" : "Who Fears Death",
+            "author" : "Nnedi Okorafor",
+            "format" : "Paperback"
+        },
+        {
+            "isbn": 23456,
+            "title": "The Name of the Wind",
+            "author": "Patrick Rothfuss",
+            "format": "Paperback"
+        },
+        {
+            "isbn": 34567,
+            "title": "The City We Became",
+            "author": "N.K. Jemisin",
+            "format": "Hardcover"
+        }
 
 ]
 
