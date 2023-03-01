9f8f8a00444451e4cbeefb163f7b3ab9cb5fd8fc
Camille Williams <camiwills325@gmail.com>
2023-03-01 15:47:02 -0500

    Adding fetch request

--- week7pp/client/src/App.js
+++ week7pp/client/src/App.js
@@ -7,6 +7,16 @@ function App() {
   const [city, setCity] = useState("");
   // const [result, setResult] = useState(null);
 
+  const sendCity = (city) => {
+    const params = new URLSearchParams({cityName: city});
+    const URL = `http://localhost:8082/weather?${params}`;
+    fetch(URL)
+    .then((response) =>  response.json)
+    .then((result) => {
+      console.log(result)
+    })
+  }
+
   // //A function to do the get request and set the state from the hard code data
   // const loadCity = () => {
   //   fetch("http://localhost:8082/api/weather")
@@ -19,6 +29,10 @@ function App() {
   //     });
   // }
 
+  const userInputCity = (city) => {
+    sendCity(city)
+    setCity(city)
+  }
  const handleSubmit = (city) =>{
   // e.preventDefault();
   setCity(city);
