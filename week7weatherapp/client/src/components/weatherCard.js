f430b5aa898843631f75d9a751076873bec3719c
Camille Williams <camiwills325@gmail.com>
2023-03-03 18:42:06 -0500

    Add sunrise and sunset

--- week7weatherapp/client/src/components/weatherCard.js
+++ week7weatherapp/client/src/components/weatherCard.js
@@ -1,6 +1,17 @@
+
 const WeatherCard = (props) => {
 console.log(props.data)
 // add data from API
+
+  let sunrise = props.data.sys.sunrise;
+  let sunset = props.data.sys.sunset;
+
+  let rise = new Date(sunrise * 1000);
+  let sunrise1 = rise.toLocaleTimeString();
+
+  let set = new Date(sunset * 1000)
+  let sunrise2 = set.toLocaleTimeString();
+
 
 // had to update props to get data from the live API, not my hardcode
     return (
@@ -33,6 +44,13 @@ console.log(props.data)
           <p>
             Wind Speed: <span className="data">
               {props.data.wind.speed} mph
+            </span>
+          </p>
+          <p>
+            Sunrise: <span className="data">
+              {sunrise1}
+            </span> Sunset <span className="data">
+              {sunrise2}
             </span>
           </p>
             </div>
